70d92e2a1a6b124d8b656e3968dee076b419bfbf
Jun Cheng <chengjun583@qq.com>
2022-02-11 11:21:16 +0800

    Delete m1_write2txt.m

--- m1_write2txt.m
+++
@@ -1,8 +0,0 @@
-% Prepare data for log-rank test and Kaplan-Meier survival curve
-
-clear
-
-tab = readtable('SurvivalData-AAC-20210901.xlsx');
-% tab.group = 1 represents treatments designed for PAC.
-
-dlmwrite('res_m1.txt', [tab.os, tab.status, tab.group], '\t');
